1245dc77a38d00460691b89977ef5f7bf3b6f854
ipekselcen <33267154+ipekselcen@users.noreply.github.com>
2026-01-08 16:19:50 +0000

    Deploying to gh-pages from @ ipekselcen/ipekselcen.github.io@395b1067899b6bcf7f3101be5397200555ebc51f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -52,18 +52,7 @@ ninja.data = [{
             window.location.href = "/blog/2025/alzheimers-epigenomics/";
           
         },
-      },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "projects-rna-seq-analysis",
+      },{id: "projects-rna-seq-analysis",
           title: 'RNA-seq Analysis',
           description: "A decision framework for experimental design and computational analysis",
           section: "Projects",handler: () => {
