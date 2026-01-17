6ffef21a795c06d4d3520c9c7be3d609e7c7dccc
ipekselcen <33267154+ipekselcen@users.noreply.github.com>
2026-01-17 22:07:40 +0000

    Deploying to gh-pages from @ ipekselcen/ipekselcen.github.io@0be10010fdffbe133be805907a73944796730d71 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,15 +30,15 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "post-bisulfite",
+        },{id: "post-",
         
-          title: "Bisulfite",
+          title: "",
         
         description: "",
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/blog/2026/bisulfite/";
+            window.location.href = "/blog/2026/2026-01-17-bisulfite/";
           
         },
       },{id: "post-what-i-wish-someone-had-told-me-about-tet1-a-computational-thinking-journey",
